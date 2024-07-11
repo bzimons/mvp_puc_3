962880987c1f4f13b1a4fb8b8e8e93d86a302c1e
Beatriz Simoes <beatrizleal@gmail.com>
2024-07-11 14:50:00 +0200

    Update: README.md catalogo_dados.md imagens notebooks

--- notebooks/mvp3/data_analysis.r
+++ notebooks/mvp3/data_analysis.r
@@ -1,7 +1,7 @@
 # Databricks notebook source
 # MAGIC %md
 # MAGIC # Etapa 3 - Análise de dados
-# MAGIC Leitura das bases do star schema e respostas das perguntas elaboradas no objetivo
+# MAGIC Leitura das bases do star-schema e respostas das perguntas elaboradas no objetivo
 
 # COMMAND ----------
 
@@ -61,3 +61,67 @@ print(paste(x1,"% compraram na mesma categoria e ",x2,"% em categorias diferente
 
 # MAGIC %md
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 3) Qual a categoria de produtos mais presente?
+# MAGIC
+
+# COMMAND ----------
+
+df <- products %>% dplyr::group_by(category) %>% dplyr::count()
+dplyr::arrange(df, desc(n))
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC O mercado é dominado pelos produtos de Casa e Cozinha, Eletronicos e Computadores.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 4) Qual a média, mediana, mínima e máxima do preço dos produtos das principais categorias?
+# MAGIC
+
+# COMMAND ----------
+
+df <- products %>%
+  dplyr::group_by(category) %>%
+  dplyr::summarise(media = mean(actual_price), mediana=median(actual_price),minimo = min(actual_price),maximo = max(actual_price), n = dplyr::n())
+  head(dplyr::arrange(df, desc(n)),4)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC O maior preço foi da categoria de Electronics, por 9373 reais, e o menor da categoria de computadores por 2,61 reais.
+# MAGIC
+# MAGIC Lembrete: Os valores originais da base estavam em rúpias indianas e foram convertidos para Reais com base na cotação do dia 01/07/2024 no notebook create_schema.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 5) Qual a porcentagem de produtos com nota de avaliação suficientemente confiável?
+# MAGIC
+# MAGIC Pelo TLC, um tamanho de amostra 30 é o suficiente para a aproximação da distribuição normal. Aqui pode-se imaginar que se o rating_count for maior que 30 as notas são confiáveis. 
+
+# COMMAND ----------
+
+df <- products %>%
+  dplyr::mutate(nota_maior_30 = ifelse(rating_count>=30,T,F)) %>%
+  dplyr::group_by(nota_maior_30) %>% dplyr::count()
+
+df$porcentagem <- round((df$n/sum(df$n))*100,2)
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Quase 94% dos produtos possuem nota com quantidade suficiente de avaliações para confiabilidade da nota
+
+# COMMAND ----------
+
+# MAGIC %md
